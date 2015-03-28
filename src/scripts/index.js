ed005451aec5b19a8762242d1f330d27de5052b8
Joshua Gross <joshua@gross.is>
2015-03-28 17:29:20 -0400

    Hover-interactive examples; auto-resizing text areas; footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,20 @@ var marked = require( 'marked' );
 
 var Parser = function( input ) {
     var name = input.dataset.name,
-        output = document.querySelector( '[data-type="output"][data-name="' + name + '"]' );
+        output = document.querySelector( '[data-type="output"][data-name="' + name + '"]' ),
+        style = window.getComputedStyle( input, null ),
+        heightOffset = parseFloat( style.paddingTop ) + parseFloat( style.paddingBottom );
 
     var markdownify = function() {
         output.innerHTML = marked( input.value );
+    };
+
+    var resize = function() {
+        console.log( input.scrollHeight, heightOffset );
+        input.style.height = 'auto';
+
+        var toHeight = input.scrollHeight - heightOffset;
+        input.style.height = toHeight + 'px';
     };
 
     if( !( this instanceof Parser ) ) {
@@ -16,6 +26,10 @@ var Parser = function( input ) {
     this.init = function() {
         input.addEventListener( 'keydown', markdownify );
         input.addEventListener( 'keyup', markdownify );
+        input.addEventListener( 'input', resize );
+        input.addEventListener( 'keyup', resize );
+
+        resize();
     };
 
     this.init();
